ed2f80f5c29f041fe0ff9a1002e86b34b9da4578
ubuntu <aitechmanml@gmail.com>
2024-10-17 15:16:50 +0530

    added hover size increase to about us html images

---
+++ assets/js/formHandler.js
@@ -0,0 +1,31 @@
+async function submitForm(event) {
+    event.preventDefault(); // Prevent default form submission (page reload)
+    
+    const form = document.getElementById('calculate');
+    const formData = {
+      name: form.name.value,
+      email: form.email.value,
+      subject: form.subject.value,
+      reason: form.chooseOption.value,
+    };
+
+    try {
+      const response = await fetch('https://backendimg.onrender.com/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        document.getElementById('form-message').innerHTML = '<p style="color: green;">Form submitted successfully!</p>';
+      } else {
+        document.getElementById('form-message').innerHTML = '<p style="color: red;">Error: ' + result.detail + '</p>';
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      document.getElementById('form-message').innerHTML = '<p style="color: red;">Failed to submit the form. Please try again later.</p>';
+    }
+  }
